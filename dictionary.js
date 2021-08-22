c0630b367cf569068e2015812d5a6e082937c8c4
abhiramkg <48902030+abhiramkg2000@users.noreply.github.com>
2021-08-22 16:31:11 +0530

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -7,48 +7,63 @@ let meaning=document.querySelector("#meaning");
 let partofspeech_heading=document.querySelector("#partofspeech_heading");
 let partofspeech_content=document.querySelector(".partofspeech_content");
 
+let audio_heading=document.querySelector("#audio_heading");
+
+let synonyms_container=document.querySelector(".synonyms_container");
+let synonyms_heading=document.querySelector("#synonyms_heading");
 function display()
 {
-    fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word.value).then(res=> res.json())
+    const result=fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+word.value)
+    .then(res=> res.json())
     .then(data=>{
-        console.log(data);
         meaning_heading.innerHTML="Meaning";
         meaning.innerHTML=data[0].meanings[0].definitions[0].definition;
         partofspeech_heading.innerHTML="Part of Speech";
         for(var i=0;i<data[0].meanings.length;i++)
         {
-            //console.log(data[0].meanings[i].partOfSpeech)
-            /*if(i<((data[0].meanings.length)-1))
-            {
-                partofspeech.innerHTML+=data[0].meanings[i].partOfSpeech+",";
-            }
-            else
-            {
-                partofspeech.innerHTML+=data[0].meanings[i].partOfSpeech;
-            }*/
             var p=document.createElement("p");
             p.setAttribute("class","partofspeech");
             p.innerHTML=data[0].meanings[i].partOfSpeech;
             partofspeech_content.append(p);
         }
-        //for(i=0;i<)
-        //synonyms.innerHTML=data[0].meanings[0].partOfSpeech+","+data[0].meanings[1].partOfSpeech;
-        //synonyms_heading.innerHTML="Synonyms";
+        audio_heading.innerHTML="How To Pronounce";
         var audio=document.createElement("audio");
         audio.setAttribute("id","audio");
         audio.setAttribute("src",data[0].phonetics[0].audio);
         audio.setAttribute("controls","controls");
         audio.setAttribute("muted","muted");
         container.append(audio);
+        synonyms_heading.innerHTML="Synonyms";
+        var synonyms_array=data[0].meanings[0].definitions[0].synonyms;
+        if(synonyms_array)
+        {
+            var synonyms_data=" ";
+            for(i=0;i<synonyms_array.length;i++)
+            {
+                synonyms_data +=`<p class="synonyms">${synonyms_array[i]}</p>`;
+            }
+            synonyms_container.innerHTML=synonyms_data;
+        }
+        if(synonyms_array.length==0){
+            synonyms_container.innerHTML="No Synonyms Available";
+        }
     })
+    .catch(error => {
+        meaning_heading.innerHTML=" ";
+        alert("Incorrect Word,Enter Again");
+    });
 }
 function reset()
 {
     let audio=document.querySelector("#audio");
+    audio.remove();
+    word.value=" ";
+    audio_heading.innerHTML=" ";
     meaning_heading.innerHTML=" ";
     meaning.innerHTML=" ";
     partofspeech_heading.innerHTML=" ";
     //synonyms_heading.innerHTML=" ";
     partofspeech_content.innerHTML=" ";
-    audio.remove();
+    synonyms_container.innerHTML=" ";
+    synonyms_heading.innerHTML=" ";
 }
