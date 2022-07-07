9add560a8eb879793ebfbc7cbe1d272e8020af0c
abhiramkg <48902030+abhiramkg2000@users.noreply.github.com>
2022-07-07 12:17:32 +0530

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,5 +1,4 @@
 let word = document.querySelector("#word");
-let container = document.querySelector(".container");
 
 let meaning_heading = document.querySelector("#meaning_heading");
 let meaning = document.querySelector("#meaning");
@@ -8,56 +7,61 @@ let partofspeech_heading = document.querySelector("#partofspeech_heading");
 let partofspeech_content = document.querySelector(".partofspeech_content");
 
 let audio_heading = document.querySelector("#audio_heading");
-let audio_container = document.querySelector(".audio_container");
+let audio_content = document.querySelector(".audio_content");
 
-let synonyms_container = document.querySelector(".synonyms_container");
 let synonyms_heading = document.querySelector("#synonyms_heading");
+let synonyms_content = document.querySelector(".synonyms_content");
 
-function display() 
-{
+function display() {
   let audio = document.getElementsByClassName("audio");
   var audio_length = audio.length;
-  while (audio_length != 0) 
-  {
+  while (audio_length !== 0) {
     audio[audio_length - 1].remove();
     audio_length--;
   }
 
-/**************************************************************************************************/
+/***********************************************************************************************/
+
   const result = fetch(
     "https://api.dictionaryapi.dev/api/v2/entries/en/" + word.value
   )
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      
       meaning_heading.innerHTML = "Meaning";
       meaning.innerHTML = data[0].meanings[0].definitions[0].definition;
       partofspeech_heading.innerHTML = "Part of Speech";
       var partofspeech_data = " ";
-      for (var i = 0; i < data[0].meanings.length; i++) 
-      {
+      for (var i = 0; i < data[0].meanings.length; i++) {
         partofspeech_data += `<p class="partofspeech">${data[0].meanings[i].partOfSpeech}</p>`;
       }
       partofspeech_content.innerHTML = partofspeech_data;
-/**************************************************************************************************/
+
+/***********************************************************************************************/
 
       audio_heading.innerHTML = "How To Pronounce";
-      var audio = document.createElement("audio");
-      audio.setAttribute("class", "audio");
-      var src = " ";
-      for (var i = 0; i < data[0].phonetics.length; i++) 
-      {
-        if (data[0].phonetics[i].audio != " ") {
-          src = data[0].phonetics[i].audio;
+      audio_content.innerHTML = " ";
+      if (data[0].phonetics.length !== 0) {
+        var audio = document.createElement("audio");
+        audio.setAttribute("class", "audio");
+        var src = " ";
+        for (i = 0; i < data[0].phonetics.length; i++) {
+          if (data[0].phonetics[i].audio !== "") {
+            src = data[0].phonetics[i].audio;
+            break;
+          }
         }
+        console.log(src);
+        audio.setAttribute("src",src);
+        audio.setAttribute("controls", "controls");
+        audio.setAttribute("muted", "muted");
+        audio_content.append(audio);
+      } else {
+        var audio_data = `<p class="error">Audio not available</p>`;
+        audio_content.innerHTML = audio_data;
       }
-      audio.setAttribute("src",src);
-      audio.setAttribute("controls", "controls");
-      audio.setAttribute("muted", "muted");
-      audio_container.append(audio);
 
-/**************************************************************************************************/
+/***********************************************************************************************/
 
       synonyms_heading.innerHTML = "Synonyms";
       var synonyms_array = data[0].meanings[0].synonyms;
@@ -66,26 +70,46 @@ function display()
         for (i = 0; i < synonyms_array.length; i++) {
           synonyms_data += `<p class="synonyms">${synonyms_array[i]}</p>`;
         }
-        synonyms_container.innerHTML = synonyms_data;
+        synonyms_content.innerHTML = synonyms_data;
       }
-      if (synonyms_array.length == 0) {
-        synonyms_container.innerHTML = "No Synonyms Available";
+      if (synonyms_array.length === 0) {
+        synonyms_data = `<p class="error">No Synonyms Available</p>`;
+        synonyms_content.innerHTML = synonyms_data;
       }
     })
     .catch((error) => {
       meaning_heading.innerHTML = " ";
+      let audio = document.getElementsByClassName("audio");
+      var audio_length = audio.length;
+      while (audio_length !== 0) {
+        audio[audio_length - 1].remove();
+        audio_length--;
+      }
+
+      word.value = " ";
+
+      audio_heading.innerHTML = " ";
+      audio_content.innerHTML = " ";
+
+      meaning_heading.innerHTML = " ";
+      meaning.innerHTML = " ";
+
+      partofspeech_heading.innerHTML = " ";
+      partofspeech_content.innerHTML = " ";
+
+      synonyms_content.innerHTML = " ";
+      synonyms_heading.innerHTML = " ";
+
       alert("Incorrect Word,Enter Again");
     });
 }
 
-/*************************************************************************************************/
+/***********************************************************************************************/
 
-function reset() 
-{
+function reset() {
   let audio = document.getElementsByClassName("audio");
   var audio_length = audio.length;
-  while (audio_length != 0) 
-  {
+  while (audio_length !== 0) {
     audio[audio_length - 1].remove();
     audio_length--;
   }
@@ -97,8 +121,8 @@ function reset()
   partofspeech_heading.innerHTML = " ";
 
   partofspeech_content.innerHTML = " ";
-  synonyms_container.innerHTML = " ";
+  synonyms_content.innerHTML = " ";
   synonyms_heading.innerHTML = " ";
 }
 
-/**************************************************************************************************/
+/***********************************************************************************************/
